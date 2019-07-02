b8be85ffe674d3428dcb94839752e263ba65e7fb
arurey <52457207+arurey@users.noreply.github.com>
2019-07-02 23:40:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,27 +25,3 @@ async def ting(ctx):
     await ctx.send('pong')
     
 bot.run(token)
-
-import discord
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-@client.event
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
-
-client.run("token")
