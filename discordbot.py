dbded1441540aa61166aa18ab11cf8ee0dc66b6f
arurey <52457207+arurey@users.noreply.github.com>
2019-07-03 00:46:13 +0900

    in out

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,7 @@ client = discord.Client()
 async def on_voice_state_update(member, before, after): 
     if member.guild.id == 589164084654637085 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(589164084667220149)
+        alert_channel = client.get_channel(595640986881753089)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
