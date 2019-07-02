d16df9191638cbd447fc8517cee1b321384d1901
arurey <52457207+arurey@users.noreply.github.com>
2019-07-03 00:42:33 +0900

    1

--- discordbot.py
+++ discordbot.py
@@ -5,23 +5,21 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+import discord
+from datetime import datetime, timedelta
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+client = discord.Client()
 
+@client.event
+async def on_voice_state_update(member, before, after): 
+    if member.guild.id == 589164084654637085 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(589164084667220149)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-@bot.command()
-async def tinn(ctx):
-    await ctx.send('pong')
-    
-@bot.command()
-async def ting(ctx):
-    await ctx.send('pong')
-    
-bot.run(token)
+client.run("NTk1NjAyNTc4Nzg0NjQ5MjE2.XRt4Eg.Yj7Fm0WJjo3e3dCcPzkLAfb6_gU")
